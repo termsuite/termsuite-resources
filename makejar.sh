e9411f1c52df6ae519c1aaac9082aea8603a6448
Damien Cram <damien.cram@univ-nantes.fr>
2016-07-26 10:47:38 +0200

    Also export rasource jar for each language

--- makejar.sh
+++ makejar.sh
@@ -4,3 +4,8 @@ find . -name '*~' -exec rm -f {} \;
 
 rm -f termsuite-resources.jar
 jar cf termsuite-resources.jar fr en ru es de LICENSE README.md META-INF
+jar cf termsuite-resources-fr.jar fr LICENSE README.md META-INF
+jar cf termsuite-resources-en.jar en LICENSE README.md META-INF
+jar cf termsuite-resources-de.jar de LICENSE README.md META-INF
+jar cf termsuite-resources-es.jar es LICENSE README.md META-INF
+jar cf termsuite-resources-ru.jar ru LICENSE README.md META-INF
